d29ba6c1fb4ac933ed7b91f0480cbd0c5a975ab8
Ilya Kotov <iokotov@astralinux.ru>
2024-05-06 22:16:54 +0300

    using dock widget buttons from common style for Qt 6.7 or higher

--- src/qt6gtk2-style/qgtkstyle.cpp
+++ src/qt6gtk2-style/qgtkstyle.cpp
@@ -674,20 +674,12 @@ int QGtkStyle::pixelMetric(PixelMetric metric,
     case PM_ToolBarItemMargin:
         return 1;
     case PM_SmallIconSize:
-#if (QT_VERSION >= QT_VERSION_CHECK(6, 7, 0))
-        return 22;
-#else
         return 16;
-#endif
     case PM_MaximumDragDistance:
         return -1;
     case PM_TabCloseIndicatorWidth:
     case PM_TabCloseIndicatorHeight:
-#if (QT_VERSION >= QT_VERSION_CHECK(6, 7, 0))
-        return 16;
-#else
         return 20;
-#endif
     default:
         return QCommonStyle::pixelMetric(metric, option, widget);
     }
@@ -4106,7 +4098,11 @@ QIcon QGtkStyle::standardIcon(StandardPixmap standardIcon,
     case SP_TitleBarNormalButton:
     case SP_TitleBarCloseButton:
     case SP_DockWidgetCloseButton:
+#if (QT_VERSION >= QT_VERSION_CHECK(6, 7, 0))
+        return QCommonStyle::standardIcon(standardIcon, option, widget);
+#else
         return QIcon(QGtkStyle::standardPixmap(standardIcon, option, widget));
+#endif
     case SP_DialogDiscardButton:
         return qt_gtk_get_icon(GTK_STOCK_DELETE);
     case SP_DialogOkButton:
